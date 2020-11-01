fe76b3f332d15a6493d164a7b5634d177f7b27df
Richard Varadi <ricsi19981007@gmail.com>
2020-11-01 13:45:05 +0100

    Add a simple backend with a counter

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const OpenAPIBackend = require('openapi-backend').default;
+const express = require('express');
+const app = express();
+
+const api = new OpenAPIBackend({
+    definition: './openapi.yml',
+});
+
+var cnt = 0;
+
+api.registerHandler('getCounter', (ctx, req, res) => {
+    cnt = cnt + 1;
+    return res.status(200).json({ counter: cnt });
+})
+
+api.init();
+
+app.use((req, res) => api.handleRequest(req, req, res));
+
+var server = app.listen(9000, () => console.info('api listening at http://localhost:9000'));
+server.close();
+module.exports.server = server;
